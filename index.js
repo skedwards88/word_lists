37db99f5287c8974edf7fcb44f581922c3f4de4e
skedwards88 <skedwards88@github.com>
2023-01-16 09:58:59 -0800

    export from main

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 import commonWords from "./compiled/commonWords.json" assert { type: "json" };
 import uncommonWords from "./compiled/uncommonWords.json" assert { type: "json" };
+
+export {commonWords}
+export {uncommonWords}
