8863b8c67701bf427ac7f03641da28d7736940ae
skedwards88 <skedwards88@github.com>
2024-02-12 07:39:02 -0800

    linter

--- compiled/compile.py
+++ compiled/compile.py
@@ -1,114 +1,117 @@
 import json
 
+
 def getCommonWords():
 
-  gutenberg = []
-  with open("processed/gutenberg.txt", "r") as file:
-    for line in file:
-      gutenberg.append(line.strip())
+    gutenberg = []
+    with open("processed/gutenberg.txt", "r") as file:
+        for line in file:
+            gutenberg.append(line.strip())
 
-  wiki = []
-  with open("processed/wiki.txt", "r") as file:
-    for line in file:
-      wiki.append(line.strip())
+    wiki = []
+    with open("processed/wiki.txt", "r") as file:
+        for line in file:
+            wiki.append(line.strip())
 
-  brysbaert = []
-  with open("processed/brysbaert.txt", "r") as file:
-    for line in file:
-      brysbaert.append(line.strip())
+    brysbaert = []
+    with open("processed/brysbaert.txt", "r") as file:
+        for line in file:
+            brysbaert.append(line.strip())
 
-  movies = []
-  with open("processed/movies.txt", "r") as file:
-    for line in file:
-      movies.append(line.strip())
+    movies = []
+    with open("processed/movies.txt", "r") as file:
+        for line in file:
+            movies.append(line.strip())
 
-  notActuallyCommon = []
-  with open("compiled/notActuallyCommon.txt", "r") as file:
-    for line in file:
-      notActuallyCommon.append(line.strip())
+    notActuallyCommon = []
+    with open("compiled/notActuallyCommon.txt", "r") as file:
+        for line in file:
+            notActuallyCommon.append(line.strip())
 
-  brysbaertUncommon = []
-  with open("processed/brysbaertUncommon.txt", "r") as file:
-    for line in file:
-      brysbaertUncommon.append(line.strip())
+    brysbaertUncommon = []
+    with open("processed/brysbaertUncommon.txt", "r") as file:
+        for line in file:
+            brysbaertUncommon.append(line.strip())
 
-  notActuallyUncommon = []
-  with open("compiled/notActuallyUncommon.txt", "r") as file:
-    for line in file:
-      notActuallyUncommon.append(line.strip())
+    notActuallyUncommon = []
+    with open("compiled/notActuallyUncommon.txt", "r") as file:
+        for line in file:
+            notActuallyUncommon.append(line.strip())
 
-  # Take words that are included in Guttenburg AND wiki
-  common = list(set(gutenberg).intersection(set(wiki)))
-  # And words that are included in movies
-  common = list(set(common).union(set(movies)))
-  # And words that are included in brysbaert
-  common = list(set(common).union(set(brysbaert)))
+    # Take words that are included in Guttenburg AND wiki
+    common = list(set(gutenberg).intersection(set(wiki)))
+    # And words that are included in movies
+    common = list(set(common).union(set(movies)))
+    # And words that are included in brysbaert
+    common = list(set(common).union(set(brysbaert)))
 
-  # Remove words from brysbaertUncommon
-  common = list(set(common).difference(set(brysbaertUncommon)))
-  
-  # And words that are included in notActuallyUncommon
-  common = list(set(common).union(set(notActuallyUncommon)))
+    # Remove words from brysbaertUncommon
+    common = list(set(common).difference(set(brysbaertUncommon)))
 
-  # Remove words from notActuallyCommon
-  common = list(set(common).difference(set(notActuallyCommon)))
+    # And words that are included in notActuallyUncommon
+    common = list(set(common).union(set(notActuallyUncommon)))
 
-  # Sort alphabetically then sort by length
-  common.sort()
-  common.sort(key=len)
+    # Remove words from notActuallyCommon
+    common = sorted(set(common).difference(set(notActuallyCommon)))
 
-  return common
+    # Sort alphabetically then sort by length
+    common.sort(key=len)
+
+    return common
+
 
 def getAllWords():
-  wordnik = []
-  with open("processed/wordnik.txt", "r") as file:
-    for line in file:
-      wordnik.append(line.strip())
+    wordnik = []
+    with open("processed/wordnik.txt", "r") as file:
+        for line in file:
+            wordnik.append(line.strip())
 
-  return wordnik
+    return wordnik
+
 
 def writeWords(path, words):
-  with open(path, "w") as file:
-    json.dump(words, file, indent=2)
-    file.writelines("\n")
+    with open(path, "w") as file:
+        json.dump(words, file, indent=2)
+        file.writelines("\n")
+
 
 def writeWordsByLength(basePath, words):
-  len2 = []
-  len3 = []
-  len4 = []
-  len5 = []
-  len6 = []
-  len7 = []
-  len8plus = []
+    len2 = []
+    len3 = []
+    len4 = []
+    len5 = []
+    len6 = []
+    len7 = []
+    len8plus = []
 
-  for word in words:
-    if len(word) <= 2:
-      len2.append(word)
-    elif len(word) == 3:
-      len3.append(word)
-    elif len(word) == 4:
-      len4.append(word)
-    elif len(word) == 5:
-      len5.append(word)
-    elif len(word) == 6:
-      len6.append(word)
-    elif len(word) == 7:
-      len7.append(word)
-    else:
-      len8plus.append(word)
+    for word in words:
+        if len(word) <= 2:
+            len2.append(word)
+        elif len(word) == 3:
+            len3.append(word)
+        elif len(word) == 4:
+            len4.append(word)
+        elif len(word) == 5:
+            len5.append(word)
+        elif len(word) == 6:
+            len6.append(word)
+        elif len(word) == 7:
+            len7.append(word)
+        else:
+            len8plus.append(word)
 
-  writeWords(f"{basePath}Len2.json", len2)
-  writeWords(f"{basePath}Len3.json", len3)
-  writeWords(f"{basePath}Len4.json", len4)
-  writeWords(f"{basePath}Len5.json", len5)
-  writeWords(f"{basePath}Len6.json", len6)
-  writeWords(f"{basePath}Len7.json", len7)
-  writeWords(f"{basePath}Len8plus.json", len8plus)
+    writeWords(f"{basePath}Len2.json", len2)
+    writeWords(f"{basePath}Len3.json", len3)
+    writeWords(f"{basePath}Len4.json", len4)
+    writeWords(f"{basePath}Len5.json", len5)
+    writeWords(f"{basePath}Len6.json", len6)
+    writeWords(f"{basePath}Len7.json", len7)
+    writeWords(f"{basePath}Len8plus.json", len8plus)
+
 
 common = getCommonWords()
 all = getAllWords()
-uncommon = list(set(common).symmetric_difference(set(all)))
-uncommon.sort()
+uncommon = sorted(set(common).symmetric_difference(set(all)))
 uncommon.sort(key=len)
 
 writeWords("compiled/commonWords.json", common)
